da80196926b892e9304e5dacf1476fca12897f14
MichaelBoegner <cmikeb@gmail.com>
2018-12-13 00:09:21 -0500

    Updates

---
+++ precache-manifest.2936dc830a184454db99bd3aacfce684.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d9df7a0ab9574e787e3",
+    "url": "/conways-life/static/js/runtime~main.8d9df7a0.js"
+  },
+  {
+    "revision": "01fc9e9d43f485151d53",
+    "url": "/conways-life/static/js/main.01fc9e9d.chunk.js"
+  },
+  {
+    "revision": "e130c10b169b6631f3da",
+    "url": "/conways-life/static/js/1.e130c10b.chunk.js"
+  },
+  {
+    "revision": "01fc9e9d43f485151d53",
+    "url": "/conways-life/static/css/main.7157a648.chunk.css"
+  },
+  {
+    "revision": "ffd31de83c7d9c88cb3bccf56d6635c9",
+    "url": "/conways-life/index.html"
+  }
+];
